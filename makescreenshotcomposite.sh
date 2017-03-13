fb3f01b9fb75d98e94c73d2a68ca1555432edd3e
kfrn <knfrances@gmail.com>
2017-03-14 09:41:58 +1300

    Finish script

--- makescreenshotcomposite.sh
+++ makescreenshotcomposite.sh
@@ -6,14 +6,13 @@ input_file=$1
 filename=$(basename "$input_file") # Gets rid of path of input file
 filename="${filename%%.*}" # Gets just filename
 clean_filename=`python3 ./fixfilename.py "$filename"` # Gets rid of spaces, brackets, etc
-echo $filename
-echo $clean_filename
+echo "Original filename was" $filename "and clean filename is" $clean_filename
 
 # Get duration of video in seconds; set number of frame captures ($divisor)
 duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input_file")
 echo "Video length in seconds:" $duration
-# divisor=71 # To make 72 frame captures
-divisor=5 # to make 6. for testing
+divisor=71 # To make 72 frame captures
+# divisor=5 # to make 6. for testing
 
 # Set interval (in seconds) and time of first screengrab ($seek_time)
 interval=$(echo "scale=0 ; $duration / $divisor" | bc)
@@ -32,6 +31,7 @@ echo "Frame captures will be saved into the subdirectory" $output_folder
 
 # Create frame captures
 ffmpeg -i "$input_file" -ss $seek_time -r 1/$interval ./$output_folder/"$clean_filename"_screencap_%02d.png -hide_banner
+echo "Frame captures made."
 
 # Depending on the video & interval, 73 frame captures may be made. Delete the 73rd if it exists
 if [[ -f ./$output_folder/"$clean_filename"_screencap_73.png ]]; then
@@ -39,16 +39,21 @@ if [[ -f ./$output_folder/"$clean_filename"_screencap_73.png ]]; then
 fi
 
 files=$(ls ./$output_folder/*.png)
-
-# Run Python script that orders image files by their primary hue
+# Run Python script that orders image files by their dominant hue
+echo "Now crunching the numbers. Stand by ..."
 colour_order=`python3 ./getdominantcolour.py $files`
-echo "files in colour order:" $colour_order
+
+# TODO?: use array directly in imagemagick command rather than making text files
+$(echo $colour_order > "$output_folder"/array.txt)
+sed -i 's/\[//; s/]//' "$output_folder"/array.txt
 
 # Make 6x12 composite image
-montage "$output_folder"/*.png -tile 6x12 -geometry +0+0 "$output_folder"/"$clean_filename"_montage_fullsize.jpg
-convert "$output_folder"/"$clean_filename"_montage_fullsize.jpg -resize 750 "$output_folder"/"$clean_filename"_montage_750w.jpg
+montage @"$output_folder"/array.txt -tile 6x12 -geometry +0+0 "$output_folder"/"$clean_filename"_montage_fullsize_sorted.jpg
+montage "$output_folder"/*.png -tile 6x12 -geometry +0+0 "$output_folder"/"$clean_filename"_montage_fullsize_orig.jpg
+convert "$output_folder"/"$clean_filename"_montage_fullsize_sorted.jpg -resize 750 "$output_folder"/"$clean_filename"_montage_750w.jpg
 echo "Composite images created."
 
 # Remove the individual frame captures
 rm ./$output_folder/*.png
-echo "Individual frame captures deleted."
+rm ./$output_folder/*.txt
+echo "Individual frame captures and text files deleted."
